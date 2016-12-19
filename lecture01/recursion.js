71c2a6e79ff2dcd989b2e6fbf97f89efdb000b9b
Michael Gagnon <mikegagnon@gmail.com>
2016-12-19 16:53:15 -0700

    Lecture 1

---
+++ lecture01/recursion.js
@@ -0,0 +1,20 @@
+
+function fibonacci(n) {
+  if (n <= 0) {
+    console.error("Fiboncci numbers are not defined when n <= 0");
+  } else if (n == 1 || n == 2) {
+    return 1;
+  } else {
+    return fibonacci(n - 1) + fibonacci(n - 2);
+  }
+}
+
+console.log(fibonacci(1));
+console.log(fibonacci(2));
+console.log(fibonacci(3));
+console.log(fibonacci(4));
+console.log(fibonacci(5));
+console.log(fibonacci(6));
+
+
+
