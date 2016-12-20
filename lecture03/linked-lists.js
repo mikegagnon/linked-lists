e2e227facfa9712f812f4f4d253b52e1c2cc6c92
Michael Gagnon <mikegagnon@gmail.com>
2016-12-20 09:35:06 -0700

    Lecture 3

---
+++ lecture03/linked-lists.js
@@ -0,0 +1,65 @@
+
+function assert(condition) {
+    if (!condition) {
+        console.error("Test failed");
+    }
+}
+
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.tail = this;
+        this.next = undefined;
+    }
+
+    append(value) {
+        if (this.next == undefined) {
+            this.next = new Node(value);
+        } else {
+            this.next.append(value);
+        }
+    }
+
+    append2(value) {
+
+        var current = this;
+
+        while (current.next != undefined) {
+            current = current.next;
+        }
+
+        current.next = new Node(value);
+    }
+}
+
+class ListWithTail {
+    constructor(){
+        this.head = undefined;
+        this.tail = undefined;
+    }
+
+    append(value) {
+        if (this.head == undefined) {
+            this.head = new Node(value);
+            this.tail = this.head;
+        } else {
+            this.tail.next = new Node(value);
+            this.tail = this.tail.next;
+        }
+    }
+}
+
+var list = new ListWithTail();
+list.append("A");
+list.append("B");
+list.append("C");
+
+assert(list.head.value == "A");
+assert(list.head.next.value == "B");
+assert(list.head.next.next.value == "C");
+assert(list.head.next.next.next == undefined);
+assert(list.tail.value == "C");
+
+
+
+
