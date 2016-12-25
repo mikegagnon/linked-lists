0a30afb3b24e4c05b11348126aca5df229eab9d6
Michael Gagnon <mikegagnon@gmail.com>
2016-12-25 00:01:53 -0500

    fibonacci

--- final/linked-lists.js
+++ final/linked-lists.js
@@ -21,6 +21,23 @@ assert(factorial(3) == 6);
 assert(factorial(4) == 24);
 assert(factorial(5) == 120);
 
+function fibonacci(n) {
+  if (n <= 0) {
+    console.error("Fiboncci numbers are not defined when n <= 0");
+  } else if (n == 1 || n == 2) {
+    return 1;
+  } else {
+    return fibonacci(n - 1) + fibonacci(n - 2);
+  }
+}
+
+assert(fibonacci(1) == 1);
+assert(fibonacci(2) == 1);
+assert(fibonacci(3) == 2);
+assert(fibonacci(4) == 3);
+assert(fibonacci(5) == 5);
+assert(fibonacci(6) == 8);
+
 class Node {
     constructor(value) {
         this.value = value;
